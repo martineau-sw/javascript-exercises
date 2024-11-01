0cb264f24c158dacfaf9a9264d987571a24cb195
Seth Martineau <martineausw@gmail.com>
2024-10-31 23:21:16 -0400

    feat: complete 04_removeFromArray exercise
    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, target, ...targets) {
+  for (const target of targets) {
+    array = array.filter(element => element !== target)
+  }
+
+  return array.filter((element) => element !== target);
 };
 
 // Do not edit below this line
