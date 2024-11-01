4040ff1fcb153b6c80d483d28efa53e989f22ae6
Seth Martineau <martineausw@gmail.com>
2024-10-31 23:09:00 -0400

    feat: complete 03_repeatString exercise
    All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, multiple) {
+  if(multiple < 0) {
+    return 'ERROR';
+  }
 
+  let repeatedString = '';
+  for (let count = 0; count < multiple; count++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
