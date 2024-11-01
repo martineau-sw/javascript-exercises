c32ed841caebe30713df580cbdbdbca7994f14c0
Seth Martineau <martineausw@gmail.com>
2024-10-31 23:13:14 -0400

    feat: complete 03_reverseString exercise
    All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversedString = '';
 
+  for (let index = 1; index <= string.length; index++) {
+    reversedString += string.at(-index);
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
