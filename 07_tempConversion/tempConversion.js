6ecee1afa3975ce3cf9c494d1cb8b32352f08206
Seth Martineau <martineausw@gmail.com>
2024-10-31 23:54:57 -0400

    feat: complete 07_tempConversion exercise
    All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheightTemperature) {
+  const celsiusTemperature = 1 / 1.8 * (fahrenheightTemperature - 32);
+  return +celsiusTemperature.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  const fahrenheightTemperature = 1.8 * celsiusTemperature + 32;
+  return +fahrenheightTemperature.toFixed(1);
 };
 
 // Do not edit below this line
