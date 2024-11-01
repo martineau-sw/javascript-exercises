68628d723f9c42cb09fa057c6dc4016533d63fb2
Seth Martineau <martineausw@gmail.com>
2024-10-31 23:41:59 -0400

    feat: complete 05_sumAll exercise
    Used partial sum equation
    All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+  if (isNaN(startNumber) || isNaN(endNumber) ||
+      startNumber < 0 || endNumber < 0 || 
+      !Number.isInteger(startNumber) || !Number.isInteger(endNumber)) {
+    return 'ERROR';
+  }
+  const numberOfTerms = Math.abs(endNumber - startNumber) + 1;
+  return numberOfTerms * (startNumber + endNumber) / 2;
 };
 
 // Do not edit below this line
