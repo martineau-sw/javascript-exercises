f6c24927352319dd6474bdf1c440d65f8e7fab51
Seth Martineau <martineausw@gmail.com>
2024-11-05 09:36:26 -0500

    feat: complete 08_calculator exercise
    All tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((accumulator, number) => {
+    return accumulator + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((accumulator, number) => {
+    return accumulator * number;
+  })
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let product = 1;
+	for (let i = 1; i <= x; i++) {
+    product *= i
+  }
+  return product;
 };
 
 // Do not edit below this line
