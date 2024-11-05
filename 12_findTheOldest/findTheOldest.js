3e1266aad54a5dddc1c09838bd5c3909db138050
Seth Martineau <martineausw@gmail.com>
2024-11-05 16:50:46 -0500

    feat: complete 12_findTheOldest exercise
    All tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let person = people.reduce((oldest, currentPerson) => {
+    return getAge(currentPerson) > getAge(oldest) ? currentPerson : oldest;
+  }, people.at(0));
+  return person;
 };
+
+function getAge(person) {
+  return (person.yearOfDeath ? person.yearOfDeath : 2024) - person.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
